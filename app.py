2ca107a7de16bf87e32e2c49b85e267de7598b96
Razil Christian <razilchristian@gmail.com>
2026-01-15 21:57:49 +0530

    updated UIUX

--- app.py
+++ app.py
@@ -485,7 +485,7 @@ with st.sidebar:
     # Logo with better styling
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        st.image("src/aadharimg/Aadhaar_Logo.svg.jpeg", 
+        st.image("src/aadharimageg/Aadhaar_Logo.jpeg", 
                  width=120)
     
     st.divider()
